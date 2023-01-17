a08311f9d24b265e3dfeb1c21c8b8dff9b6fa86a
Дмитрий Соколов <socolov-2609@yandex.ru>
2023-01-17 22:36:03 +0300

    17.01

--- yatube_api/api/urls.py
+++ yatube_api/api/urls.py
@@ -1,4 +1,18 @@
-from django.urls import path
+from rest_framework import routers
+from django.urls import include, path
+from api.views import PostViewSet, GroupViewSet, CommentViewSet, FollowViewSet
+
+
+router = routers.DefaultRouter()
+router.register('v1/posts', PostViewSet, basename='posts')
+router.register(
+    r'v1/posts/(?P<post_id>\d+)/comments', CommentViewSet, basename='comments'
+)
+router.register('v1/groups', GroupViewSet, basename='groups')
+router.register('v1/follow', FollowViewSet, basename='followers')
 
 urlpatterns = [
+    path('', include(router.urls)),
+    path('auth/', include('djoser.urls')),
+    path('auth/', include('djoser.urls.jwt')),
 ]
--- yatube_api/api/views.py
+++ yatube_api/api/views.py
@@ -1,1 +1,58 @@
 # TODO:  Напишите свой вариант
+from rest_framework import viewsets
+from rest_framework.pagination import PageNumberPagination
+from rest_framework import serializers
+from django.core.exceptions import PermissionDenied
+
+from posts.models import Group, Post, Comment, Follow
+from .serializers import GroupSerializer, PostSerializer, CommentSerializer, FollowSerializer
+
+class PostViewSet(viewsets.ModelViewSet):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+    pagination_class = PageNumberPagination
+
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user)
+
+    def perform_update(self, serializer):
+        if serializer.instance.author != self.request.user:
+            raise PermissionDenied('Изменение чужого контента запрещено!')
+        super(PostViewSet, self).perform_update(serializer)
+
+    def perform_destroy(self, serializer):
+        if serializer.author != self.request.user:
+            raise PermissionDenied('Удаление чужого контента запрещено!')
+        super(PostViewSet, self).perform_destroy(serializer)
+
+class GroupViewSet(viewsets.ModelViewSet):
+    queryset = Group.objects.all()
+    serializer_class = GroupSerializer
+
+
+class CommentViewSet(viewsets.ModelViewSet):
+    queryset = Comment.objects.all()
+    serializer_class = CommentSerializer
+    pagination_class = PageNumberPagination
+
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user)
+
+    def perform_update(self, serializer):
+        if serializer.instance.author != self.request.user:
+            raise PermissionDenied('Изменение чужого контента запрещено!')
+        super(CommentViewSet, self).perform_update(serializer)
+
+    def perform_destroy(self, serializer):
+        if serializer.author != self.request.user:
+            raise PermissionDenied('Удаление чужого контента запрещено!')
+        super(CommentViewSet, self).perform_destroy(serializer)
+
+
+class FollowViewSet(viewsets.ModelViewSet):
+    queryset = Follow.objects.all()
+    serializer_class = FollowSerializer
+    pagination_class = PageNumberPagination
+
+    def perform_create(self, serializer):
+        serializer.save(Following=self.request.user)
