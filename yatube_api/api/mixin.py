01d7ea79c63a7fce5163537551236ccbacda8aa4
Дмитрий Соколов <socolov-2609@yandex.ru>
2023-01-23 22:39:05 +0300

    23.01

--- yatube_api/api/mixin.py
+++ yatube_api/api/mixin.py
@@ -2,6 +2,6 @@ from rest_framework import mixins, viewsets
 
 
 class PackMixin(mixins.CreateModelMixin,
-              mixins.ListModelMixin,
-              viewsets.GenericViewSet):
+                mixins.ListModelMixin,
+                viewsets.GenericViewSet):
     pass
